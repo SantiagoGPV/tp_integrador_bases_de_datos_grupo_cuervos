772aecdc43aea110257dc2515c1654af4170552a
Oliva Josue <113141002+Oliver2003-UTN@users.noreply.github.com>
2026-06-13 01:13:44 -0300

    Update 04_triggers.sql
    Casi se termina el primer trigger :P
    
    Tano dice: "Hola Santi"

--- sql/04_triggers.sql
+++ sql/04_triggers.sql
@@ -1,14 +1,36 @@
 USE tpIntegradorCuervos;
 
+DELIMITER //
 -- Recalcula stock_disponible en LIBRO según el estado del préstamo.
 CREATE TRIGGER trg_actualizar_stock_insert
 AFTER INSERT ON prestamo FOR EACH ROW 
   BEGIN 
-     UPDATE libro SET stock_disponible = stock_disponible - 1
+     UPDATE libro SET stock_disponible = stock_disponible - 1 
      WHERE isbn = (SELECT isbn FROM ejemplar WHERE id_ejemplar = NEW.id_ejemplar);
      -- EXPLICACIÓN DEL WHERE:
      -- Como la tabla 'prestamo' solo tiene el 'id_ejemplar' (NEW.id_ejemplar),
      -- usamos esa subconsulta para viajar a la tabla 'ejemplar', buscar a qué libro 
      -- pertenece ese ejemplar, bajarnos su 'isbn' y así poder actualizar el libro correcto.
 END; 
+DELIMITER ;
+
+
+DELIMITER //
+-- Recalcula stock_disponible luego de actualizar el prestamo
+CREATE TRIGGER trg_actualizar_stock_update
+AFTER UPDATE ON prestamo FOR EACH ROW 
+BEGIN 
+    -- Si el estado cambia a 'DEVUELTO', el libro vuelve a estar en la estantería
+    IF OLD.estado != 'DEVUELTO' AND NEW.estado = 'DEVUELTO' THEN  
+        UPDATE libro SET stock_disponible = stock_disponible + 1
+        WHERE isbn = (SELECT isbn FROM ejemplar WHERE id_ejemplar = NEW.id_ejemplar);
+    END IF;
+   -- Si el estado pasa de 'DEVUELTO' a 'NOT DEVUELTO'
+    IF OLD.estado = 'DEVUELTO' AND NEW.estado != 'DEVUELTO' THEN   
+        UPDATE libro 
+        SET stock_disponible = stock_disponible - 1
+        WHERE isbn = (SELECT isbn FROM ejemplar WHERE id_ejemplar = NEW.id_ejemplar);  -- ('ACTIVO', 'DEVUELTO', 'VENCIDO')
+    END IF;
+END
+
 DELIMITER ;
